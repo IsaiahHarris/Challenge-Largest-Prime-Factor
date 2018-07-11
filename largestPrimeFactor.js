7cef9f0601007e76f87d2fdaabf3ba4f9af15140
Isaiah Harris <harrisfambam@gmail.com>
2018-07-11 10:31:02 -1000

    complete

--- largestPrimeFactor.js
+++ largestPrimeFactor.js
@@ -1,6 +1,20 @@
-exports.largestPrimeFactor = function(n){
+'use strict'
+exports.largestPrimeFactor = function (n) {
   var primeNumber = 0;
-  // do your work here
+  let primeFactors = [];
+
+  for (let i = 0; i < n; i++) {
+    if (n % i === 0) {
+      primeFactors.push(i);
+      n /= i;
+    }
+  }
+
+  if (n > 0) {
+    primeFactors.push(n);
+  }
+
+  primeNumber = primeFactors.pop();
 
   return primeNumber;
 };
